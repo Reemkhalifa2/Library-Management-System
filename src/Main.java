9fcc03794ac922ed0b0f0c0bce6a7a1e46bf8d62
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 11:27:43 +0400

    Add menu navigation and user input handling

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import Menu.Menu;
+import Utilities.Constants;
+import Utilities.MenuMessage;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Menu menu = new Menu();
+        Boolean exit = false;
+        while(exit){
+            menu.displayMenu();
+            System.out.println(Constants.ENTER_OPTION);
+            switch (scanner.nextInt()){
+                case 1->{
+                    Boolean LibraryExit = false;
+                    while (LibraryExit){
+                        System.out.println(MenuMessage.LibraryService);
+                        System.out.println(Constants.ENTER_OPTION);
+
+
+                    }
+
+                }
+                case 2->{
+                    exit = true;
+                }
+            }
+
+        }
+
 
     }
 }
