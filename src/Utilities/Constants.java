d6d59ed45423a8b705a020110a6381466e141de0
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 14:58:09 +0400

    Add constant for item not found message

--- src/Utilities/Constants.java
+++ src/Utilities/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     static public String ITEM_ADDED_SUCCESSFULLY = "Item added successfully!";
     static public String ITEM_ADDED_FAILED = "Item adding Failed";
     static public String ENTER_OPTION = "Enter your option: ";
+    static public String ITEM_NOT_FOUND = "Items not found";
 }
