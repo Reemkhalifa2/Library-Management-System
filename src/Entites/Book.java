f6829cab5b5ce03e1359ebed9d1c5c19547ee941
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 10:11:23 +0400

    feat: Add Book class with properties

---
+++ src/Entites/Book.java
@@ -0,0 +1,13 @@
+package Entites;
+
+public class Book extends Item{
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private String author;
+}
