8c488ff50642cfee7c881667125d5686ce169c0d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 16:05:26 +0400

    Update menu options in MenuMessage.java for clarity and consistency

--- src/Utilities/MenuMessage.java
+++ src/Utilities/MenuMessage.java
@@ -4,16 +4,20 @@ public class MenuMessage {
     static public String mainMenu = """
             1- Library Service.
             2- Member Service.
+            3- Exit
             """;
     static public String LibraryService = """
             1- Add Item
             2- Search Item
             3- Display all items
+            4- Exit
             """;
     static public String MemberMenu = """
-            Register member
-                Find member
-                List members
+            1- Register member
+            2- Find member
+            3- List members
+            4- Exit
+
             """;
 
 
