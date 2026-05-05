e8eae73f37f36a8efba1b4884fa7fe88ecfde39d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 15:41:14 +0400

    Update Constants

--- src/Utilities/Constants.java
+++ src/Utilities/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     static public String ITEM_ADDED_FAILED = "Item adding Failed";
     static public String ENTER_OPTION = "Enter your option: ";
     static public String ITEM_NOT_FOUND = "Items not found";
+    static public String MEMBER_REGISTERED = "user is now a member!";
 }
