9a32fe11318410a5534b4e561d545a06937db6c4
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 14:07:27 +0400

    Refactor main loop and integrate LibraryService

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Menu.Menu;
+import Services.LibraryService;
 import Utilities.Constants;
 import Utilities.MenuMessage;
 
@@ -8,26 +9,26 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        LibraryService libraryService = new LibraryService();
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("am worling");
         Menu menu = new Menu();
-        Boolean exit = false;
-        while(exit){
+        Boolean Continue = true;
+        while(Continue){
             menu.displayMenu();
             System.out.println(Constants.ENTER_OPTION);
             switch (scanner.nextInt()){
                 case 1->{
-                    Boolean LibraryExit = false;
+                    Boolean LibraryExit = true;
                     while (LibraryExit){
                         System.out.println(MenuMessage.LibraryService);
                         System.out.println(Constants.ENTER_OPTION);
-
-
+                        LibraryExit = libraryService.handleLibraryMenu(scanner.nextInt());
                     }
 
                 }
                 case 2->{
-                    exit = true;
+                    Continue = false;
                 }
             }
 
