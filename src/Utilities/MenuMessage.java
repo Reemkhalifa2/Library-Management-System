8be78b3ae4544ab195b358d47b1ce6c1e63479c6
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 16:27:21 +0400

    Update menu messages

--- src/Utilities/MenuMessage.java
+++ src/Utilities/MenuMessage.java
@@ -2,17 +2,23 @@ package Utilities;
 
 public class MenuMessage {
     static public String mainMenu = """
+            Library management system
+            
             1- Library Service.
             2- Member Service.
             3- Exit
             """;
     static public String LibraryService = """
+            Library Service
+            
             1- Add Item
             2- Search Item
             3- Display all items
             4- Exit
             """;
     static public String MemberMenu = """
+            Member Service
+            
             1- Register member
             2- Find member
             3- List members
