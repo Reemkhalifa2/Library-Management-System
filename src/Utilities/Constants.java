cb8b6ab20459684ff3059b087841ff0d4c6b2335
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 16:59:22 +0400

    Update Constants

--- src/Utilities/Constants.java
+++ src/Utilities/Constants.java
@@ -6,5 +6,6 @@ public class Constants {
     static public String ENTER_OPTION = "Enter your option: ";
     static public String ITEM_NOT_FOUND = "Items not found";
     static public String MEMBER_REGISTERED = "user is now a member!";
+    static public String MEMBER_NOT_FOUND= "Member not found";
     static public String INPUT_EXIT_CONTINUE_MESSAGE_MEMBER = "press q to exit, Enter for registering more members";
 }
