8c110d006b36b7678122b0906fa1599d1f0533dc
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 10:15:03 +0400

    Add getters and setters for Item properties

--- src/Entites/Item.java
+++ src/Entites/Item.java
@@ -2,6 +2,31 @@ package Entites;
 
 public class Item {
     private Integer ID ;
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
     private String title;
     private Boolean status;
 
