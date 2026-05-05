e0e68321447c6b3b72e7ccef590ee7d5bd3f35a1
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-05 10:14:37 +0400

    feat: Add Magazines class with properties

---
+++ src/Entites/Magazines.java
@@ -0,0 +1,14 @@
+package Entites;
+
+public class Magazines{
+    public Integer getIssueNumber() {
+        return issueNumber;
+    }
+
+    public void setIssueNumber(Integer issueNumber) {
+        this.issueNumber = issueNumber;
+    }
+
+    private Integer issueNumber;
+
+}
