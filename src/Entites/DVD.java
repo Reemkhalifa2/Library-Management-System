2f2977777c08a2b12f239aa406f3d66f69c156b1
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 09:35:02 +0400

    Add director property with getter and setter in DVD class

--- src/Entites/DVD.java
+++ src/Entites/DVD.java
@@ -1,5 +1,14 @@
 package Entites;
 
 public class DVD extends Item{
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    private String director;
 
 }
